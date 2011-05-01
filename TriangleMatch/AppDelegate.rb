db97f533fbea3770f367c88ec4369d17ed314ac7
kyab <kyossi212@gmail.com>
2011-05-01 11:27:16 +0900

    Initial Commit

---
+++ TriangleMatch/AppDelegate.rb
@@ -0,0 +1,15 @@
+#
+#  AppDelegate.rb
+#  TriangleMatch
+#
+#  Created by koji on 11/05/01.
+#  Copyright 2011 __MyCompanyName__. All rights reserved.
+#
+
+class AppDelegate
+	attr_accessor :window
+	def applicationDidFinishLaunching(a_notification)
+		# Insert code here to initialize your application
+	end
+end
+
